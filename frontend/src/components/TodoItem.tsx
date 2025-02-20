ad521e3befd5fdac1b7787f7f85405ec42e03b31
Omar-Ballesteros <omar.ballesteros@encora.com>
2025-02-19 21:51:40 -0600

    Implemented set as Done and Undone functionalities

--- frontend/src/components/TodoItem.tsx
+++ frontend/src/components/TodoItem.tsx
@@ -1,5 +1,7 @@
 import { Todo } from "../types/todo";
 import { useModalContext } from "../context/ModalContext";
+import { useState } from "react";
+import { setAsDone, setAsUnDone } from "../api/api";
 
 interface TodoItemProps {
   todo: Todo;
@@ -7,15 +9,35 @@ interface TodoItemProps {
 
 export default function TodoItem({ todo }: TodoItemProps) {
   const { setOpen, setId } = useModalContext();
+  const [isDone, setIsDone] = useState(todo.done);
+
   const handleEdit = () => {
     setOpen(true);
     setId(todo.id);
   };
+
+  const handleToggleDone = async () => {
+    try {
+      if (isDone) {
+        await setAsUnDone(todo.id);
+      } else {
+        await setAsDone(todo.id);
+      }
+      setIsDone(!isDone);
+    } catch (error) {
+      console.error("Error changing todo state", error);
+    }
+  };
+
   return (
     <tr className=" bg-white hover:bg-gray-100">
       <td className="p-4 border border-gray-400">
-        {" "}
-        <input type="checkbox" className="scale-150" />{" "}
+        <input
+          type="checkbox"
+          className="scale-150"
+          checked={isDone}
+          onChange={handleToggleDone}
+        />
       </td>
       <td className="p-4 border border-gray-400">{todo.todoText}</td>
       <td className="p-4 border border-gray-400">{todo.priority}</td>
